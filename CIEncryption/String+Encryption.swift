6e9b30ac0396b064b2f0de499de1d4b4430cc8bd
Tharindu Madushanka <tharindufit@gmail.com>
2021-09-14 07:50:31 +0530

    Blowfish Encryption

--- CIEncryption/String+Encryption.swift
+++ CIEncryption/String+Encryption.swift
@@ -143,4 +143,13 @@ extension String {
         return rc
     }
     
+    func base64Encoded() -> String? {
+        return data(using: .utf8)?.base64EncodedString()
+    }
+
+    func base64Decoded() -> String? {
+        guard let data = Data(base64Encoded: self) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+    
 }
--- CIEncryptionTests/CIEncryptionTests.swift
+++ CIEncryptionTests/CIEncryptionTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import CryptoSwift
 @testable import CIEncryption
 
 class CIEncryptionTests: XCTestCase {
@@ -14,6 +15,7 @@ class CIEncryptionTests: XCTestCase {
     var encMsg: String = "" // AES encrypted message to decrypt
     var encMsg2: String = "" // DES encrypted message to decrypt
     var encMsg3: String = "" // 3DES encrypted message to decrypt
+    var encMsg4: String = "" // Blowfish encrypted message to decrypt
     var password: String = "En3r9pt10nK5y" //"En3r9pt10nK5y886En3r9pt1" // ex. En3r9pt10nK5y
     var encKey: String = "" // SHA-256 hash of password
     
@@ -38,10 +40,14 @@ class CIEncryptionTests: XCTestCase {
         {
             encMsg3 = try! String(contentsOf: path)
         }
+        if let path = bundle.url(forResource: "decmsg4", withExtension: "txt")
+        {
+            encMsg4 = try! String(contentsOf: path)
+        }
         // Hash password
         encKey = password.sha256()!
         print("Encryption Key:\(encKey)")
-        print("--------------------------")
+        print("--------------------------------")
     }
 
     override func tearDownWithError() throws {
@@ -51,6 +57,7 @@ class CIEncryptionTests: XCTestCase {
     /// AES Encryption & Decryptions
 
     func testAESEncryption() throws {
+        print("------------AES-------------")
         print("Msg:\(msg) Size:\(msg.count)")
         self.measure {
             // measures AES encryption time
@@ -60,6 +67,7 @@ class CIEncryptionTests: XCTestCase {
     }
     
     func testAESDecryption() throws {
+        print("------------AES Decrypt-------------")
         print("EncMsg:\(encMsg) Size:\(encMsg.count)")
         self.measure {
             // measures AES decryption time
@@ -71,6 +79,7 @@ class CIEncryptionTests: XCTestCase {
     /// DES Encryption & Decryptions
     
     func testDESEncryption() throws {
+        print("------------DES-------------")
         print("Msg:\(msg) Size:\(msg.count)")
         self.measure {
             // measures DES encryption time
@@ -80,7 +89,8 @@ class CIEncryptionTests: XCTestCase {
     }
     
     func testDESDecryption() throws {
-        print("EncMsg:\(encMsg) Size:\(encMsg.count)")
+        print("------------DES Decrypt-------------")
+        print("EncMsg:\(encMsg2) Size:\(encMsg2.count)")
         self.measure {
             // measures DES decryption time
             let val = encMsg2.desDecrypt(key: encKey)
@@ -91,6 +101,7 @@ class CIEncryptionTests: XCTestCase {
     /// DES Encryption & Decryptions
     
     func test3DESEncryption() throws {
+        print("------------3DES-------------")
         print("Msg:\(msg) Size:\(msg.count)")
         self.measure {
             // measures 3DES encryption time
@@ -100,11 +111,42 @@ class CIEncryptionTests: XCTestCase {
     }
     
     func test3DESDecryption() throws {
-        print("EncMsg:\(encMsg) Size:\(encMsg.count)")
+        print("------------3DES Decrypt-------------")
+        print("EncMsg:\(encMsg3) Size:\(encMsg3.count)")
         self.measure {
             // measures 3DES decryption time
             let val = encMsg3.tripleDesDecrypt(key: encKey)
             print("Decrypted:\(val!)")
+        }
+    }
+    
+    /// Blowfish Encryption & Decryptions
+    
+    func testBlowfishEncryption() throws {
+        print("------------Blowfish-------------")
+        print("Msg:\(msg) Size:\(msg.count)")
+        self.measure {
+            // measures 3DES encryption time
+            let bytes = try! Blowfish(key: encKey.bytes, padding: .pkcs7).encrypt(msg.bytes)
+            let val = bytes.toBase64()
+            print("Enc Val:\(val) Size:\(val.count)")
+//            let dec = try! Blowfish(key: encKey.bytes, padding: .pkcs7).decrypt(Data(base64Encoded: val)!.bytes)
+//            if let decryptedString = String(data: Data(dec), encoding: .utf8) {
+//                print("DEC: \(decryptedString)")
+//            } else {
+//                print ("Failed!")
+//            }
+        }
+    }
+    
+    func testBlowfishDecryption() throws {
+        print("------------Blowfish Decrypt-------------")
+        print("Msg:\(encMsg4) Size:\(encMsg4.count)")
+        self.measure {
+            // measures 3DES encryption time
+            let val = try! Blowfish(key: encKey.bytes, padding: .pkcs7).decrypt(Data(base64Encoded: encMsg4)!.bytes)
+            let decrypted = String(data: Data(val), encoding: .utf8)!
+            print("Decrypted:\(decrypted)")
         }
     }
 
